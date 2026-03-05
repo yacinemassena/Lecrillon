daaecaae2c0e9896a7b405eb207de2a6d1d76011
Yacine Massena <yacinemassena@gmail.com>
2026-03-05 20:52:58 +0400

    Fix sweep OOM: use --num-workers 0 for parallel experiments

--- sweep_lr.sh
+++ sweep_lr.sh
@@ -20,6 +20,7 @@ EPOCHS=5
 TRAIN_STEPS=50
 VAL_STEPS=20
 SEQ_LEN=15000
+NUM_WORKERS=0  # Use 0 workers to prevent OOM with 6 parallel processes
 
 # Learning rates to test (6 values for 6 GPUs)
 LR_VALUES=(1e-3 5e-4 1e-4 5e-5 1e-5 1e-6)
@@ -55,6 +56,7 @@ for i in {0..5}; do
             --train-steps $TRAIN_STEPS \
             --val-steps $VAL_STEPS \
             --seq-len $SEQ_LEN \
+            --num-workers $NUM_WORKERS \
             --exp-name $EXP_NAME \
             --results-csv $RESULTS_CSV \
             --checkpoint-dir "checkpoints_${EXP_NAME}" \
@@ -67,6 +69,7 @@ for i in {0..5}; do
             --train-steps $TRAIN_STEPS \
             --val-steps $VAL_STEPS \
             --seq-len $SEQ_LEN \
+            --num-workers $NUM_WORKERS \
             --exp-name $EXP_NAME \
             --results-csv $RESULTS_CSV \
             --checkpoint-dir "checkpoints_${EXP_NAME}" \
